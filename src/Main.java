a37dd122ad33c537992cbec22b235ac99dfb7cda
Nacho Serrano <ignaserrano@gmail.com>
2024-09-30 20:37:54 +0200

    Added sum method and tested

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to our calculator");
+        Operators operators = new Operators();
+
+        System.out.println(operators.add(5, 5));
 
     }
 }
--- src/Operators.java
+++ src/Operators.java
@@ -1,2 +1,6 @@
 public class Operators {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
 }
